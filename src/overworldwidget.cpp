a771b5a7daa8bdb1ab524a316d4f5564ae906237
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-11 02:04:03 +0200

    Add QRadialGradient include to overworldwidget.cpp

--- src/overworldwidget.cpp
+++ src/overworldwidget.cpp
@@ -11,6 +11,7 @@
 #include <QFileInfo>
 #include <QDebug>
 #include <QRandomGenerator>
+#include <QRadialGradient>
 
 // ═════════════════════════════════════════════════════════════════════════════
 //  PlayerSprite
