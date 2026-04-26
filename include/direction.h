c92116969c03c9d0fd87f6d995e343fc8cf12175
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-26 23:13:54 +0300

    Add Direction enum class for movement directions

---
+++ include/direction.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum class Direction : int
+{
+    Right        = 0,
+    Up           = 1,
+    ForwardRight = 2,
+    ForwardLeft  = 3,
+    Down         = 4,
+    DownRight    = 5,
+    DownLeft     = 6,
+    Left         = 7
+};
