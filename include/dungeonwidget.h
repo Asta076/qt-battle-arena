d773dcb91c7351e857b528e93f8b53c46e81a988
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-26 23:42:53 +0300

    Refactor DungeonWidget and related classes

--- include/dungeonwidget.h
+++ include/dungeonwidget.h
@@ -1,23 +1,68 @@
 #pragma once
 
 #include <QWidget>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
 #include <QGraphicsRectItem>
+#include <QGraphicsEllipseItem>
+#include <QGraphicsTextItem>
 #include <QTimer>
+#include <QKeyEvent>
 #include <QSet>
 #include <QList>
+#include <QPixmap>
 
 #include "character.h"
+#include "goldhudwidget.h"
 #include "playercontroller.h"
+#include "overworldwidget.h"   // Direction, walkRow(), idleCol()
 
-class QGraphicsView;
-class QGraphicsScene;
-class QResizeEvent;
-class QKeyEvent;
-class GoldHudWidget;
 class AudioManager;
 class PauseOverlayWidget;
 
-class EnemySprite : public QGraphicsRectItem
+struct DungeonSpriteSheet
+{
+    QPixmap pixmap;
+
+    static constexpr int FRAME_W = 68;
+    static constexpr int FRAME_H = 68;
+
+    QPixmap frame(int col, int row) const
+    {
+        return pixmap.copy(col * FRAME_W, row * FRAME_H, FRAME_W, FRAME_H);
+    }
+};
+
+class DungeonPlayerSprite : public QGraphicsPixmapItem
+{
+public:
+    explicit DungeonPlayerSprite(const DungeonSpriteSheet& sheet,
+                                 QGraphicsItem* parent = nullptr);
+
+    static constexpr qreal W = 96.0;
+    static constexpr qreal H = 96.0;
+
+    void updateAnimation(bool isMoving, Direction facingDir);
+
+private:
+    void setWalkAnim(Direction dir);
+    void setIdleFrame(Direction dir);
+    void applyFrame();
+
+    const DungeonSpriteSheet& m_sheet;
+    bool m_isIdle = true;
+    Direction m_facing = Direction::Down;
+    int m_frameIndex = 0;
+    int m_tickAccum = 0;
+
+    static constexpr int TICKS_PER_FRAME = 5;
+    static constexpr int WALK_FRAMES = 6;
+
+    QGraphicsEllipseItem* m_shadow = nullptr;
+};
+
+class EnemySprite : public QGraphicsPixmapItem
 {
 public:
     explicit EnemySprite(CharacterType type, const QString& name,
@@ -30,13 +75,21 @@ public:
     QString enemyName() const { return m_name; }
 
     void patrol(const QRectF& worldBounds);
+    void updateAnimation();
 
 private:
     CharacterType m_type;
     QString m_name;
 
-    qreal m_vx;
-    qreal m_vy;
+    QPixmap m_sprite;
+    QGraphicsEllipseItem* m_shadow = nullptr;
+    QGraphicsTextItem* m_nameLabel = nullptr;
+
+    qreal m_vx = 0;
+    qreal m_vy = 0;
+
+    int m_frameIndex = 0;
+    int m_tickAccum = 0;
 };
 
 class DungeonWidget : public QWidget
@@ -70,7 +123,8 @@ private:
     QGraphicsScene* m_scene = nullptr;
     QGraphicsView* m_view = nullptr;
     GoldHudWidget* m_goldHud = nullptr;
-    QGraphicsRectItem* m_player = nullptr;
+
+    DungeonPlayerSprite* m_player = nullptr;
     QGraphicsRectItem* m_exitZone = nullptr;
 
     QList<EnemySprite*> m_enemies;
@@ -81,9 +135,11 @@ private:
     static constexpr int WORLD_W = 800;
     static constexpr int WORLD_H = 600;
 
-    static constexpr qreal PW = 28;
-    static constexpr qreal PH = 36;
+    static constexpr qreal PW = DungeonPlayerSprite::W;
+    static constexpr qreal PH = DungeonPlayerSprite::H;
     static constexpr int SPEED = 3;
+
+    DungeonSpriteSheet m_sheet;
 
     void buildScene();
     void placePlayer();
@@ -93,7 +149,6 @@ private:
     void checkCollisions();
     void fitView();
     void togglePause();
-    void buildPauseOverlay();
 
     AudioManager* m_audio = nullptr;
     PauseOverlayWidget* m_pauseOverlay = nullptr;
